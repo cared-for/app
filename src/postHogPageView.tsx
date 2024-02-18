44d57edb907d6fa31e59172fab803b5ca441ce90
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-02-18 21:30:24 +0900

    updated

--- src/postHogPageView.tsx
+++ src/postHogPageView.tsx
@@ -4,7 +4,7 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { usePostHog } from 'posthog-js/react';
 
-export default function PostHogPageView() {
+export function PostHogPageView() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const posthog = usePostHog();
