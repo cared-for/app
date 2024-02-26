3f93e5e33eeaed41b669b3df51ac317c37e5a152
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-02-26 14:47:51 +0900

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.NEXT_PUBLIC_HOST ?? "https://www.caredfor.care",
+  siteUrl: "https://www.caredfor.care",
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
