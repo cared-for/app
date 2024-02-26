18a4e1c8b11d9bdb66d5fd044d79c66e8d585607
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-02-26 13:54:39 +0900

    removed redundant title

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -90,11 +90,13 @@ export default async function PostPage({ params }: PostPageProps) {
           <Icons.chevronLeft className="mr-2 h-4 w-4" />
           See all posts
         </Link>
+        {/*
         <div>
           <h1 className="mt-2 inline-block font-heading text-4xl leading-tight lg:text-5xl">
             {post.title}
           </h1>
         </div>
+        */}
         <Mdx code={post.body.code} />
         <hr className="mt-12" />
         <div className="flex justify-center py-6 lg:py-10">
